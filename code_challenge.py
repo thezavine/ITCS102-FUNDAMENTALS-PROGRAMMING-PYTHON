642624c76666afa953bd97acc5751562d5cb5a95
Azeth Arvine Brandes <azetharvine@gmail.com>
2025-08-11 23:17:41 +0800

    Delete code_challenge.py

--- code_challenge.py
+++
@@ -1,2 +0,0 @@
-name = input("What's your name: ")
-print("\t\t\t\t*\n\t\t\t*\t\t*\n\t\t*\t\t\t\t*\n\t*\t\t\tHI\t\t\t*\n*\t\t\t\tBRANDES\t\t\t\t*\n\t*\t\t\t\t\t\t*\n\t\t*\t\t\t\t*\n\t\t\t*\t\t*\n\t\t\t\t*")
