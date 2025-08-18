a5f6c0b99e945e3f2d32f4efafbe273a543ee448
Azeth Arvine Brandes <azetharvine@gmail.com>
2025-08-18 11:22:21 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,8 @@
+username = 'azeth'
+password = 'animeislife'
+
+#print(username == 'azeth')
+#print((username == 'azeth') and (password == 'animeislife'))
+#print((username == 'azeth') or (password == 'animeislife'))
+print(not((username == 'azeth') and (password == 'animeislife')))
+
