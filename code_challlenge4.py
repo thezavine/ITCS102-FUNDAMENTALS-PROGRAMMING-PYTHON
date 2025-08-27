928d42ba3b038c39a4311cc24d33ab6e6f0576e9
Azeth Arvine Brandes <azetharvine@gmail.com>
2025-08-27 11:24:47 +0800

    Add files via upload

---
+++ code_challlenge4.py
@@ -0,0 +1,9 @@
+#user must input a number 
+#program will identify if number is ODD or EVEN.
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+	print("The number is Even")
+else:
+	print("The number is Odd")
