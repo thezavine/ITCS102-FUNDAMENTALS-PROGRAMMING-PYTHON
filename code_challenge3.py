e43488f69a61fc42ece4e251ba039bfa4bc02b8f
Azeth Arvine Brandes <azetharvine@gmail.com>
2025-08-27 11:22:57 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,15 @@
+#set a username and password variable.
+#user must login using these information both username & password should match
+#if username & password match prompt message 'access granted', otherwise 'access denied'
+
+username = 'yuzuhzea'
+password = 'pretty'
+
+uname = input("Enter username: ")
+pinput = input("Enter password: ")
+
+if username == username and password == pinput: 
+	print("Acces Granted")
+else:
+	print("Access Denied")
+
