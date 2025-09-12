efec69aa7c243f96a01fd21b1e6ad194a6337d1f
Azeth Arvine Brandes <azetharvine@gmail.com>
2025-09-12 11:30:35 +0800

    activity12.py

---
+++ activity12.py
@@ -0,0 +1,12 @@
+#print hello world 
+print('hello world')
+
+
+#print hello world 10 times
+#stopping point is not included on the count
+for ikot in range(1,10,1):
+    print('hello world')
+
+#another example
+for ikot in range(1,27):
+    print(ikot, ' - yuzu loves to read and watch anime')
