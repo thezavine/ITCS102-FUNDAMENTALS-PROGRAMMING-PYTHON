601fdfd223d2327e49b53d0f667d612301946384
Azeth Arvine Brandes <azetharvine@gmail.com>
2025-09-17 13:16:21 +0800

    code_challenge9.py

---
+++ code_challenge9.py
@@ -0,0 +1,14 @@
+#Simple countdown
+#user-specified number down to 1
+#COuntdown Timer Stimulator
+#27 to # 1 
+
+print("COUNTDOWN TIMER STIMULATOR")
+tim = eval(input("Enter the starting number for countdown: "))
+
+print("\nCountdown started:")
+
+for x in range(tim, 0, -1):
+    print(x)
+
+print("LIFTOFF!!")
