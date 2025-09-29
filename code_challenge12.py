7ae7de5b18609582e4f7b04e1155fc25cb08fb23
Azeth Arvine Brandes <azetharvine@gmail.com>
2025-09-29 14:26:16 +0800

    Add files via upload

---
+++ code_challenge12.py
@@ -0,0 +1,11 @@
+
+
+for e in range(1, 10, 1):
+    for f in range(10 , e, -1):
+        print(' ', end=" ")
+    for g in range(e, 0 ,-1):
+        print(g, end= " ")
+    for h in range (2, e + 1 ,1):
+        print(h, end =" ")
+    print()
+    
