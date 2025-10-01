47a9ac66e17e416a60a3451296f8787667b3a54a
Azeth Arvine Brandes <azetharvine@gmail.com>
2025-10-01 23:08:09 +0800

    Add files via upload

---
+++ code_challenge13.py
@@ -0,0 +1,60 @@
+#Christmas TREE
+
+
+for a in range(1,3,1):         #Diamond
+    for b in range(11,a,-1):
+        print(" ",end=" ") 
+    for c in range(0,a,1):
+        print(".",end=" ") 
+    for d in range(1,a,1):
+        print(".",end=" ")
+    for e in range(0,a,-1):
+        print(" ",end=" ")
+    print()
+for f in range(3,0,-1):         #First TRIANGL
+    for g in range(11,f,-1):
+        print(" ",end=" ") 
+    for h in range(0,f,1):
+        print(".",end=" ")
+    for i in range(1,f,1):
+        print(".",end=" ")
+    for j in range(1,10,1):
+        print(" ",end=" ")
+    print()
+
+for k in range(1,9,1):     #First TRIANGLE
+    for l in range(11,k,-1):
+        print(" ",end=" ") 
+    for m in range(0,k,1):
+        print("-",end=" ") 
+    for n in range(1,k,1):
+        print("-",end=" ")
+    for o in range(12,k,1):
+        print(" ",end=" ")
+    print()
+for p in range(1,11,1):     #Second TRIANGLE
+    for q in range(11,p,-1):
+        print(" ",end=" ") 
+    for r in range(0,p,1):
+        print("-",end=" ") 
+    for s in range(1,p,1):
+        print("-",end=" ")
+    for t in range(12,p,1):
+        print(" ",end=" ")
+    print()
+for u in range(1,12,1):     #Third part of the TRIANGLE
+    for v in range(11,u,-1):
+        print(" ",end=" ") 
+    for w in range(0,u,1):
+        print("-",end=" ") 
+    for x in range(1,u,1):
+        print("-",end=" ")
+    for y in range(12,u,1):
+        print(" ",end=" ")
+    print()
+for z in range(1,6,1):
+    for a in range(8,0,-1):
+        print(" ",end=" ") 
+    for b in range(4, 0 ,-1):
+        print("|",end=" ") 
+    print()
