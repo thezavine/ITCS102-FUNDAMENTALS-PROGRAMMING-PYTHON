982e9d682ebd463fd37b59c2d21ad43aa2ed7a99
Azeth Arvine Brandes <azetharvine@gmail.com>
2025-11-12 21:40:58 +0800

    dictionary

---
+++ activity26.py
@@ -0,0 +1,5 @@
+highschool_sub = ['English', 'Filipino', 'Math', 'Science', 'MAPEH', 'Araling Panlipunan']
+
+highschool_sub2 = {'easy to learn': 'English','slightly hard to understand': 'Filipino','Very hard but it is my favorite sub': 'Math','Hard': 'Science','VERY EASY': 'MAPEH','I get sleepy listening to this': 'Araling Panlipunan'}
+
+print(highschool_sub2['VERY EASY'])
