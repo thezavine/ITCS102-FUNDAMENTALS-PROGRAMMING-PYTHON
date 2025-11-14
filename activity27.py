27c1562013cd0eb83326eed59992b23ecf35a215
Azeth Arvine Brandes <azetharvine@gmail.com>
2025-11-14 11:14:12 +0800

    adding data

---
+++ activity27.py
@@ -0,0 +1,17 @@
+
+
+for a in range(1,11,1):
+    for b in range(10,a,-1):
+        print(" ",end="")
+    for c in range(1,2*a, 1):
+        print("*",end="")
+
+    print()
+
+
+
+#numbers
+for d in range(1,10,1):
+    for e in range(1,d +1,1):
+        print(d,end="")
+    print()
